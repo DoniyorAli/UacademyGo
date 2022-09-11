1fae9bba5f92bdab38ea6d0868dcc56ff72c06a8
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-09-11 11:21:51 +0500

    Identify is Odd and is Even numbers

--- GoTasks/task2.go
+++ GoTasks/task2.go
@@ -2,15 +2,6 @@ package main
 
 import "fmt"
 
-// Identify is Odd and is Even numbers
-func isOdd(a int) bool {
-	return a%2 == 0
-}
-
-func isEven(b int) bool {
-	return b%2 != 0
-}
-
 func main() {
 
 	num1, num2 := 0, 0
@@ -21,5 +12,14 @@ func main() {
 	fmt.Scanf("%d", &num2)
 	fmt.Println(num1, "is odd: ", isOdd(num1))
 	fmt.Println(num2, "is even: ", isEven(num2))
+}
 
+//*function===============================================
+// Identify is Odd and is Even numbers
+func isOdd(a int) bool {
+	return a%2 == 0
+}
+
+func isEven(b int) bool {
+	return b%2 != 0
 }
