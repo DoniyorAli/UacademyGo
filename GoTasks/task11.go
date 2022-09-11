2d8f903c3cd6d6a4fffcf619c4f3c0bb35a3256a
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-09-11 11:15:47 +0500

    I made triangle figures

---
+++ GoTasks/task11.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x string = "#"
+
+	for i := 10; i >= 1; i-- {
+		for j := 1; j <= 10; j++ {
+			if i <= j {
+				fmt.Printf("%v ", x)
+			}else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+/*
+	    	 # 
+			# # 
+		   # # # 
+		  # # # # 
+		 # # # # # 
+		# # # # # # 
+	   # # # # # # # 
+	  # # # # # # # # 
+	 # # # # # # # # # 
+	# # # # # # # # # # 
+*/
--- GoTasks/task9.go
+++ GoTasks/task9.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func main() {
 
-	// var x string = "#"
-
 	for i := 1; i <= 10; i++ {
 		for j := 1; j <= 10; j++ {
 			if i <= j {
